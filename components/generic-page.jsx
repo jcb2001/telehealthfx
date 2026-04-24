2e78293d373ed20ae32999f7ce516399a17b7600
Josh <jcburrows@gmail.com>
2026-04-24 16:24:43 -0500

    Update blog path to use /blog prefix

--- components/generic-page.jsx
+++ components/generic-page.jsx
@@ -14,7 +14,7 @@ function GenericPage({ pageSlug, onStartQuiz, priceAnchor }) {
       case 'science': return <Science />;
       case 'pricing': return <Pricing priceAnchor={priceAnchor} onStartQuiz={onStartQuiz} />;
       case 'faq': return <FAQ />;
-      case 'berberine': return <BlogBerberine />;
+      case 'blog/berberine': return <BlogBerberine />;
     }
 
     // 2. Check for Legal/Support/Company text pages
@@ -39,7 +39,7 @@ function GenericPage({ pageSlug, onStartQuiz, priceAnchor }) {
         <div className="container">
           <h1 className="serif" style={{ fontSize: 48, marginBottom: 24 }}>Page Not Found</h1>
           <p style={{ color: 'var(--ink-2)' }}>The page you are looking for does not exist.</p>
-          <button className="btn btn-primary" onClick={() => window.location.href = '?'} style={{ marginTop: 24 }}>
+          <button className="btn btn-primary" onClick={() => window.location.href = '/'} style={{ marginTop: 24 }}>
             Return Home
           </button>
         </div>
